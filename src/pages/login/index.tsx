9dec9bbf01c5ed1bb44444fa7b0f43c4614ed2ac
Jhonny Dias <ojhonny1@hotmail.com>
2024-04-01 11:04:32 -0400

    feat: create form login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,20 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { FormEvent, useState } from "react";
 
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    console.log({
+      email: email,
+      password: password,
+    });
+  }
+
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -11,6 +25,30 @@ export function Login() {
           </span>
         </h1>
       </Link>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-xl flex
+       flex-col px-2"
+      >
+        <Input
+          type="email"
+          placeholder="Digite o seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua senha..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white"
+        >
+          Acessar
+        </button>
+      </form>
     </div>
   );
 }
